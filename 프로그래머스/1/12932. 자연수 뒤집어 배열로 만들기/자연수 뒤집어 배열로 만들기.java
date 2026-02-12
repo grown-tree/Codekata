748e224abb4776e55bf595cc77636f6eb69e9852
grown-tree <92420848+grown-tree@users.noreply.github.com>
2026-02-12 09:26:26 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(long n) {
+        
+        int[] answer = new int[String.valueOf(n).length()];
+        
+        for(int i=0;0<n;i++){
+            answer[i] =(int)(n%10);
+            n /= 10;
+        }
+        
+        
+        return answer;
+    }
+}
