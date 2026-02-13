a7793ec17686308406c1f2367f91e270fc30a66c
grown-tree <92420848+grown-tree@users.noreply.github.com>
2026-02-13 09:14:47 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 6.82 ms, Memory: 92.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        
+        System.out.println("answer"+answer);
+        return answer;
+    }
+}
