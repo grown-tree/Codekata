e739af03f19781a3908057c0f7b86f3038b72f9c
grown-tree <92420848+grown-tree@users.noreply.github.com>
2026-03-31 09:46:28 +0900

    [level 1] Title: 최소직사각형, Time: 12.47 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[][] sizes) {
+             
+        int maxWidth = 0;
+        int maxHeight = 0;
+
+        for (int[] size : sizes) {
+            int w = Math.max(size[0], size[1]);
+            int h = Math.min(size[0], size[1]);
+
+            if (w > maxWidth) {
+                maxWidth = w;
+            }
+
+            if (h > maxHeight) {
+                maxHeight = h;
+            }
+        }
+        System.out.println("w,h"+maxWidth+","+maxHeight);
+        return maxWidth*maxHeight;
+    
+    }
+}
