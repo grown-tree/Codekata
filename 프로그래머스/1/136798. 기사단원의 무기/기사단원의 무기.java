c7d61831e3b90a30253b7d6ffb3a74fff414a7d2
grown-tree <92420848+grown-tree@users.noreply.github.com>
2026-05-07 09:47:58 +0900

    [level 1] Title: 기사단원의 무기, Time: 470.26 ms, Memory: 57.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        
+        int totalWeight = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int count = 0;
+
+            for (int j = 1; j * j <= i; j++) {
+                if (j * j == i) {
+                    count++;
+                } else if (i % j == 0) {
+                    count += 2;
+                }
+            }
+
+            if (count > limit) {
+                totalWeight += power;
+            } else {
+                totalWeight += count;
+            }
+        }
+
+        return totalWeight;
+    }
+}
